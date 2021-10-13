ff653c5adfa2c07c413cd33801aa57db01d10c83
Alexis Guzman <alexisguzman157@outlook.com>
2021-10-12 22:31:34 -0400

    updated server.js , now runs local hose

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+//const routes = require('./routes')
+const logger = require('morgan');
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000;
+
+const db = require('./models');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(require('./routes/api/index.js'));
+app.use(require('./routes/html/index.js'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+useNewUrlParser: true,
+useFindAndModify: false,
+useCreateIndex: true,
+useUnifiedTopology: true
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+
+  module.exports = db;
